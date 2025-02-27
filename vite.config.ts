6f7ad9896f11cad5764054ac44f2fffdb4c50d68
mrSom3a <mrsom3a8@gmail.com>
2025-02-27 15:03:38 +0400

    fix a bug

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base:"/vite-website/"
+  base:"/Test-I-O-TECH/"
 })
